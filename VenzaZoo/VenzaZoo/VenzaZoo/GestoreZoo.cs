01a4d4dfeafed20a10ddf12a8d95cabb6e1c0fc9
AlessioVenza <alessiovenza1@gmail.com>
2025-10-22 11:45:44 +0200

    I added a class called GestoreZoo.

---
+++ VenzaZoo/VenzaZoo/VenzaZoo/GestoreZoo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VenzaZoo
+{
+    class GestoreZoo
+    {
+        public string NomeZoo { get; set; }
+        public int BudgetMensile { get; set; }
+        public int NumeroVisitatori { get; set; }
+    }
+}
