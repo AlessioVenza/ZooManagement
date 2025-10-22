e5f239c03e450aa32f0ec2a2e17ed6a9e0e501d1
AlessioVenza <alessiovenza1@gmail.com>
2025-10-22 11:23:58 +0200

    Visual Studio Project files added

---
+++ VenzaZoo/VenzaZoo/VenzaZoo/Animale.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VenzaZoo
+{
+    class Animale
+    {
+        public string Nome { get; set; }
+        public string Specie { get; set; }
+        public int Eta { get; set; }
+        public int Peso { get; set; }
+        public string StatoDiSalute { get; set; }
+        public DateOnly DataArrivo { get; set; }
+        public int AnimaliAlloZoo { get; set; }
+
+
+        private double _pesoIniziale;
+        private int _numeroControlliVeterinari;
+
+        private double CalcolaVariazionePesoPercentuale()
+        {
+            return _pesoIniziale - Peso;
+        }
+
+        public double CalcolaCiboDiarioKg()
+        {
+            return 0;
+        }
+        public double CalcolaCostoGestioneMensile()
+        {
+            return 0;
+        }
+        public string EmettiVerso()
+        {
+            return "";
+        }
+        public double AggiornaPeso(double nuovoPeso)
+        {
+            return nuovoPeso = 0;
+        }
+
+    }
+}
